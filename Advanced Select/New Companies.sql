7fbb3f6ebd70194fc6058a37bd47b466640720a0
kimjueun028 <jueuniiiju@naver.com>
2025-07-14 22:31:48 +0900

    Modify New Companies.sql for description

--- Advanced Select/New Companies.sql
+++ Advanced Select/New Companies.sql
@@ -12,3 +12,8 @@ LEFT JOIN Manager M on C.company_code = M.company_code
 LEFT JOIN Employee E on C.company_code = E.company_code
 GROUP BY C.company_code, C.founder
 ORDER BY C.company_code
+
+/*
+DISTINCT : 고유한 값을 반환
+SELECT절에 있는 컬럼은 GROUP BY에 포함하거나 집계 함수로 묶여야 함 -- C.founder
+*/
